c8b5a7406eb6639564f707de7389cbef9dbdcc34
KaiL4eK <darklexi494@gmail.com>
2018-01-25 12:39:05 +0300

    Tried linear SVM

--- check_hog.py
+++ check_hog.py
@@ -7,17 +7,19 @@ import math as m
 import numpy as np
 from skimage import feature, color, data, exposure
 
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn import neighbors
+from sklearn import svm
 
-from random import randrange
+from sklearn.model_selection import KFold
+
+import random
 import time
 
 main_path = 'data/signs_only'
+neg_path = 'data/negative'
 
 data = []
 labels = []
-
-tests = []
 
 time_measure_sum   = 0
 time_measure_times = 0
@@ -29,66 +31,94 @@ for sign_type in sign_names:
 	print('Sign type: %s' % sign_type)
 	sign_imgs = os.listdir(os.path.join(main_path, sign_type))
 
-	idx = 0
-
 	for sign_img in sign_imgs:
 
 		img = cv2.imread(os.path.join(main_path, sign_type, sign_img))
 
-		img = color.rgb2grey(img)
-
+		img_grey = color.rgb2grey(img)
+		img_grey = exposure.equalize_hist(img_grey)
+		
 		start = time.time()
-		fd = feature.hog(img, orientations=8, pixels_per_cell=(ppc, ppc),
+		fd = feature.hog(img_grey, orientations=8, pixels_per_cell=(ppc, ppc),
                     	 cells_per_block=(2, 2), transform_sqrt=True)
 		end = time.time()
 		time_measure_sum += end - start
 		time_measure_times += 1
 
-		# fd, hog_image = feature.hog(img, orientations=8, pixels_per_cell=(8, 8),
-                    				# cells_per_block=(2, 2), visualise=True, transform_sqrt=True)
+		data.append(fd)
+		labels.append(sign_type)
 
-		# hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 255))
-		# hog_image_rescaled = hog_image_rescaled.astype('uint8')
+neg_imgs = os.listdir(neg_path)
+for neg_img in neg_imgs:
+	for i in range(1):
+		img = cv2.imread(os.path.join(neg_path, neg_img))
 
-		# cv2.imshow('1', np.hstack( (img, hog_image) ))
-		# cv2.imshow('2', hog_image)
-		# cv2.waitKey(10)
+		img_height, img_width, channels = img.shape
 
-		if idx == randrange(0, len(sign_imgs)):
-			tests.append( (img, sign_type) )
-		else:
-			data.append(fd)
-			labels.append(sign_type)
+		portion_size = (64, 64)
 
-		idx += 1
+		x1 = random.randint(0, img_height-portion_size[0]-1)
+		y1 = random.randint(0, img_height-portion_size[1]-1)
 
-model = KNeighborsClassifier(n_neighbors=3, weights='distance')
-model.fit(data, labels)
+		x2, y2 = x1+portion_size[0], y1+portion_size[1]
 
-for test_case in tests:
-	img = test_case[0]
-	label = test_case[1]
+		part = img[y1:y2, x1:x2]
 
-	img = color.rgb2grey(img)
+		img_grey = color.rgb2grey(part)
+		img_grey = exposure.equalize_hist(img_grey)
 
-	start = time.time()
-	fd = feature.hog(img, orientations=8, pixels_per_cell=(ppc, ppc),
-					 cells_per_block=(2, 2), transform_sqrt=True)
-	end = time.time()
-	time_measure_sum += end - start
-	time_measure_times += 1
+		start = time.time()
+		fd = feature.hog(img_grey, orientations=8, pixels_per_cell=(ppc, ppc),
+	                	 cells_per_block=(2, 2), transform_sqrt=True)
+		end = time.time()
+		time_measure_sum += end - start
+		time_measure_times += 1
 
-	pred = model.predict(fd.reshape(1, -1))[0]
+		data.append(fd)
+		labels.append('negative')
 
-	print(pred.title())
-	if label != pred.title().lower():
-		print('Achtung!!!')
+print(np.unique(labels))
 
-	cv2.imshow('1', img)
-	cv2.waitKey(0)
+# http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+# model = neighbors.KNeighborsClassifier(n_neighbors=3, weights='distance')
+
+# http://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
+model = svm.LinearSVC()
+
+# http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
+kf = KFold(n_splits=3, shuffle=True)
+for train_index, test_index in kf.split(data):
+	# print("\nTRAIN:", train_index, "\nTEST:", test_index)
+	X_train, X_test = [data[i] for i in train_index],   [data[i] for i in test_index]
+	y_train, y_test = [labels[i] for i in train_index], [labels[i] for i in test_index]
+
+	model.fit( X_train, y_train )
+	print( model.score( X_test, y_test ) )
+
+# for test_case in tests:
+# 	img = test_case[0]
+# 	label = test_case[1]
+
+# 	img = color.rgb2grey(img)
+
+# 	start = time.time()
+# 	fd = feature.hog(img, orientations=8, pixels_per_cell=(ppc, ppc),
+# 					 cells_per_block=(2, 2), transform_sqrt=True)
+# 	end = time.time()
+# 	time_measure_sum += end - start
+# 	time_measure_times += 1
+
+# 	pred = model.predict(fd.reshape(1, -1))[0]
+
+# 	print(pred.title())
+# 	if label != pred.title().lower():
+# 		print('Achtung!!!')
+
+# 	cv2.imshow('1', img)
+# 	cv2.waitKey(0)
 
 
-print( time_measure_sum / time_measure_times, ' s' )
+print( 'Hog mean time: %fs' % (time_measure_sum / time_measure_times) )
 
 exit(0)
 
